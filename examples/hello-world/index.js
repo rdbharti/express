336756955256bcde8de58c5c7922325bf42b9c65
rdbharti <rana_durlabh_bharti@yahoo.co.in>
2023-03-25 14:50:03 +0530

    Checking image push

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World.. test');
+  res.send('This is pushing the image');
 });
 
 /* istanbul ignore next */
